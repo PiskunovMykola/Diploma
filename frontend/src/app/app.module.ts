3e90bbb76b9cc927ed9ddbea7e9db3629f1d3786
Piskunov <nikolajpiskunov14@gmail.com>
2024-03-01 19:51:31 +0100

    Use HTTP Request

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,25 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ProjectCardComponent } from './project/project-card/project-card.component';
 import { ProjectListComponent } from './project/project-list/project-list.component';
+import { ProjectingService } from './services/projecting.service';
+import { AddProjectComponent } from './project/add-project/add-project.component';
+import { ProjectDetailComponent } from './project/project-detail/project-detail.component';
+
+const appRoutes: Routes = [
+  {path: '', component: ProjectListComponent},
+  {path: 'sell-project', component: ProjectListComponent},
+  {path: 'add-project', component: AddProjectComponent},
+  {path: 'project-detail/:id', component: ProjectDetailComponent},
+  {path: '**', component: ProjectListComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     ProjectCardComponent,
-    ProjectListComponent
+    ProjectListComponent,
+    AddProjectComponent,
+    ProjectDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    ProjectingService
   ],
   bootstrap: [AppComponent]
 })
