92154279ead7ac452a48b02dbb33dc3ebd24c3ad
Piskunov <nikolajpiskunov14@gmail.com>
2024-03-01 19:34:31 +0100

    Add Project List

--- frontend/src/app/project/project-list/project-list.component.ts
+++ frontend/src/app/project/project-list/project-list.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { ProjectingService } from '../../services/projecting.service';
+import { Component, OnInit } from '@angular/core';
+import { IProject } from '../IProject.interface';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
   styleUrl: './project-list.component.css'
 })
-export class ProjectListComponent {
+export class ProjectListComponent implements OnInit {
+  Sell = 1;
+  projects: Array<IProject> = [];
 
+  constructor(private route: ActivatedRoute, private projectingService: ProjectingService) { }
+
+  ngOnInit(): void {
+    if(this.route.snapshot.url.toString()){
+      this.Sell = 2;
+    }
+    this.projectingService.getAllProjects(this.Sell).subscribe(
+      data=>{
+      this.projects=data;
+      console.log(data);
+      }, error => {
+        console.log('httperror:')
+        console.log(error);
+      }
+    );
+  }
 }
