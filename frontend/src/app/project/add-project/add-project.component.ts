10c4203f4a6cfb93639a6c76a6d866da7af18116
Piskunov <nikolajpiskunov14@gmail.com>
2024-03-09 23:13:09 +0100

    Add Form for new Project

--- frontend/src/app/project/add-project/add-project.component.ts
+++ frontend/src/app/project/add-project/add-project.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,7 +8,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-project.component.css']
 })
 export class AddProjectComponent implements OnInit {
-
+  @ViewChild('Form') addProjectForm: NgForm | undefined;
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -16,6 +17,11 @@ export class AddProjectComponent implements OnInit {
   onBack()
   {
     this.router.navigate(['/']);
+  }
+
+  onSubmit(){
+    console.log('Form submitted');
+    console.log(this.addProjectForm);
   }
 
 }
