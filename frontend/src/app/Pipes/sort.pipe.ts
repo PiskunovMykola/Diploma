a904cd384f568b7e3c733903fb99472a29afb24e
Mykola Piskunov <nikolajpiskunov14@gmail.com>
2025-12-15 19:13:20 +0100

    fix sort and filter

--- frontend/src/app/Pipes/sort.pipe.ts
+++ frontend/src/app/Pipes/sort.pipe.ts
@@ -5,7 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SortPipe implements PipeTransform {
 
-  transform(value: Array<string>, args: any[]): any {
+  // Исправили тип: Array<any> вместо Array<string>, так как мы сортируем объекты
+  transform(value: Array<any>, args: any[]): any {
     const sortField = args[0];
     const sortDirection = args[1];
     let multiplier = 1;
@@ -14,16 +15,23 @@ export class SortPipe implements PipeTransform {
       multiplier = -1;
     }
 
+    if (!value || !sortField) {
+        return value;
+    }
+
     value.sort((a: any, b: any) => {
-      if (a[sortField] < b[sortField]) {
+      // Добавлена проверка, чтобы не падать на null/undefined
+      const valA = a[sortField] || 0; // Если поля нет, считаем как 0 (для чисел) или '' (для строк)
+      const valB = b[sortField] || 0;
+
+      if (valA < valB) {
         return -1 * multiplier;
-      } else if (a[sortField] > b[sortField]) {
+      } else if (valA > valB) {
         return 1 * multiplier;
       } else {
         return 0;
       }
-    }
-    );
+    });
 
     return value;
   }
